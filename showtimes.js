a313802365a8a6ce7c4481fd915f7805d3dfc0c7
Sira <SiraSaklertwilai@gmail.com>
2016-11-09 11:01:00 +0700

    Edit showtime model

--- showtimes.js
+++ showtimes.js
@@ -1,35 +1,51 @@
 var theatre = [
     {
         "id": 1,
-        "name": "Theatre 1",
+        "theatreName": "Theatre 1",
         "cinemaId": 1,
         "movieID": 1,
+        "name": "The BFG(1)",
+        "poster": "https://upload.wikimedia.org/wikipedia/en/thumb/a/af/The_BFG_poster.jpg/220px-The_BFG_poster.jpg",
+        "time": "117",
+        "director": "Steven Spielberg",
         "language": "TH/TH",
-        "time": ["13.30","15.00","17.30","20.00"]
+        "showTime": ["13.30","15.00","17.30","20.00"]
     },
     {
         "id": 2,
-        "name": "Theatre 2",
+        "theatreName": "Theatre 2",
         "cinemaId": 1,
         "movieID": 2,
+        "name": "The BFG(2)",
+        "poster": "https://upload.wikimedia.org/wikipedia/en/thumb/a/af/The_BFG_poster.jpg/220px-The_BFG_poster.jpg",
+        "time": "117",
+        "director": "Steven Spielberg",
         "language": "TH/TH",
-        "time": ["13.30","15.00","17.30","20.00"]
+        "showTime": ["13.30","15.00","17.30","20.00"]
     },
     {
         "id": 3,
-        "name": "Theatre 3",
+        "theatreName": "Theatre 3",
         "cinemaId": 1,
         "movieID": 3,
+        "name": "The BFG(3)",
+        "poster": "https://upload.wikimedia.org/wikipedia/en/thumb/a/af/The_BFG_poster.jpg/220px-The_BFG_poster.jpg",
+        "time": "117",
+        "director": "Steven Spielberg",
         "language": "TH/TH",
-        "time": ["13.30","15.00","17.30","20.00"]
+        "showTime": ["13.30","15.00","17.30","20.00"]
     },
     {
         "id": 1,
-        "name": "Theatre 1",
+        "theatreName": "Theatre 1",
         "cinemaId": 2,
         "movieID": 1,
+        "name": "The BFG(1)",
+        "poster": "https://upload.wikimedia.org/wikipedia/en/thumb/a/af/The_BFG_poster.jpg/220px-The_BFG_poster.jpg",
+        "time": "117",
+        "director": "Steven Spielberg",
         "language": "TH/TH",
-        "time": ["13.30","15.00","17.30","20.00"]
+        "showTime": ["13.30","15.00","17.30","20.00"]
     }
 ];
 
